8e0067258a09d632c7160d79347733c219f26ae4
Mintu-Das1 <mintudas1577@gmail.com>
2024-09-29 02:23:00 +0600

    added toggle features

---
+++ js/features.js
@@ -0,0 +1,16 @@
+// toggle feature (redirect button to button)
+
+document.getElementById('btn-cash-out-form').addEventListener('click',function(){
+    
+    document.getElementById('cash-out-form').classList.remove('hidden');
+
+    document.getElementById('add-money-form').classList.add('hidden');
+});
+
+
+document.getElementById('btn-add-money-form').addEventListener('click',function(){
+
+    document.getElementById('add-money-form').classList.remove('hidden');
+
+    document.getElementById('cash-out-form').classList.add('hidden');
+});
